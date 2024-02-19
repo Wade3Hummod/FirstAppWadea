4066d5e2227185b4ae25954f262fd9502309b025
Admin <ibraheemafifi02@gmail.com>
2024-02-19 09:42:06 +0200

    fgh

--- app/src/main/java/com/example/firstapp/SignUpActivity.java
+++ app/src/main/java/com/example/firstapp/SignUpActivity.java
@@ -13,12 +13,14 @@ import com.example.firstapp.data.usersTable.MyUser;
 import com.example.firstapp.data.usersTable.MyUserQuery;
 //import com.google.android.gms.tasks.OnCompleteListener;
 //import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 //import com.google.firebase.auth.AuthResult;
 //import com.google.firebase.auth.FirebaseAuth;
 
-public class  SignUpActivity extends AppCompatActivity
-{
+public class  SignUpActivity extends AppCompatActivity {
     private Button btnSave;
     private Button btnCancel;
     private TextInputEditText etName;
@@ -41,7 +43,10 @@ public class  SignUpActivity extends AppCompatActivity
         etRe_password = findViewById(R.id.etRe_password);
     }
 
-            public void onClickSave(View view) {checkSignUpSave();}
+    public void onClickSave(View view) {
+        checkSignUpSave_FB();
+    }
+
     private void checkSignUpSave_FB() {
         boolean isAllok = true; //يفحص الحقول ان كانت سليمة
         //استخراج النص من حقل الايميل
@@ -63,124 +68,115 @@ public class  SignUpActivity extends AppCompatActivity
             etEmail.setError("Wrong Email");
 
         }
-        if (password.length() < 8 || password.length() > 20 || password.contains(" ") == true)
-        {
+        if (password.length() < 8 || password.length() > 20 || password.contains(" ") == true) {
             isAllok = false;
             etPassword.setError("Password between 8 - 20 letters");
         }
-        if (!repassword.equals(password))
-        {
+        if (!repassword.equals(password)) {
             isAllok = false;
             etRe_password.setError("should be the same password");
         }
 
 
-        if (phone.length() <10 || phone.contains(" ") == true)
-        {
+        if (phone.length() < 10 || phone.contains(" ") == true) {
             isAllok = false;
             etPhone.setError("phone number is not 10  numbers");
 
         }
 
 
-        if(isAllok)
-        {
-//            //
-//            FirebaseAuth auth=FirebaseAuth.getInstance();
-//            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                @Override
-//                public void onComplete(@NonNull Task<AuthResult> task) {
-//                    if (task.isSuccessful()) {
-//                        Toast.makeText(SignUpActivity.this, "Signing up Succeeded", Toast.LENGTH_SHORT).show();
-//                        finish();
-//                    } else {
-//                        Toast.makeText(SignUpActivity.this, "Signing up Failed", Toast.LENGTH_SHORT).show();
-//                        etEmail.setError(task.getException().getMessage());
-//                    }
-//
-//                }
-//
-//
-//            });
+        if (isAllok) {
+
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SignUpActivity.this, "Signing up Succeeded", Toast.LENGTH_SHORT).show();
+
+                        finish();
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Signing up Failed", Toast.LENGTH_SHORT).show();
+                        etEmail.setError(task.getException().getMessage());
+                    }
+
+                }
+
+
+            });
 
         }
     }
 
-            private void checkSignUpSave() {
-                boolean isAllok = true; //يفحص الحقول ان كانت سليمة
-                //استخراج النص من حقل الايميل
-                String email = etEmail.getText().toString();
-                //استخراج نص كلمه المرور
-                String password = etPassword.getText().toString();
-                //استخراج نص اعادة كلمه المرور
-                String repassword = etRe_password.getText().toString();
-                // استخراج نص من رقم هاتف
-                String phone = etPhone.getText().toString();
-                //استخراج نص لأسمك
-                String name = etName.getText().toString();
+    private void checkSignUpSave() {
+        boolean isAllok = true; //يفحص الحقول ان كانت سليمة
+        //استخراج النص من حقل الايميل
+        String email = etEmail.getText().toString();
+        //استخراج نص كلمه المرور
+        String password = etPassword.getText().toString();
+        //استخراج نص اعادة كلمه المرور
+        String repassword = etRe_password.getText().toString();
+        // استخراج نص من رقم هاتف
+        String phone = etPhone.getText().toString();
+        //استخراج نص لأسمك
+        String name = etName.getText().toString();
 
-                //فحص الايمل ان كان طوله اقل من 6 او لا يحوي @ فهو خطأ
-                if (email.length() < 6 || email.contains("@") == false) {
-                    //تعديل المتغير ليدل على ان الفحص يهطي نتيجه خاطئه
-                    isAllok = false;
-                    // عرض ملاحظه خطا على الشاشه داخل حقل البريد
-                    etEmail.setError("Wrong Email");
+        //فحص الايمل ان كان طوله اقل من 6 او لا يحوي @ فهو خطأ
+        if (email.length() < 6 || email.contains("@") == false) {
+            //تعديل المتغير ليدل على ان الفحص يهطي نتيجه خاطئه
+            isAllok = false;
+            // عرض ملاحظه خطا على الشاشه داخل حقل البريد
+            etEmail.setError("Wrong Email");
 
-                }
-                if (password.length() < 8 || password.length() > 20 || password.contains(" ") == true)
-                {
-                    isAllok = false;
-                    etPassword.setError("Password between 8 - 20 letters");
-                }
-                if (!repassword.equals(password))
-                {
-                    isAllok = false;
-                    etRe_password.setError("should be the same password");
-                }
+        }
+        if (password.length() < 8 || password.length() > 20 || password.contains(" ") == true) {
+            isAllok = false;
+            etPassword.setError("Password between 8 - 20 letters");
+        }
+        if (!repassword.equals(password)) {
+            isAllok = false;
+            etRe_password.setError("should be the same password");
+        }
 
 
-                if (phone.length() <10 || phone.contains(" ") == true)
-                {
-                    isAllok = false;
-                    etPhone.setError("phone number is not 10  numbers");
+        if (phone.length() < 10 || phone.contains(" ") == true) {
+            isAllok = false;
+            etPhone.setError("phone number is not 10  numbers");
 
-                }
-
-
-                if (isAllok) {
-                    Toast.makeText(this, "All Ok", Toast.LENGTH_SHORT).show();
-                    AppDatabase db = AppDatabase.getDB(getApplicationContext());
-                    MyUserQuery usersQuery = db.getMyUserQuery();
-                    //فحص هل البريد الالكتروني موجود من قبل اي تم تسجيل من قبل
-                    if (usersQuery.checkEmail(email) != null) {
-                        etEmail.setError("found Email");
-                    } else// ان لم يكن موجودا نقوم ببناء كاءن للمستعمل وادخاله في الجدول Myuser المستعملين
-                    {
-                        // بناء الكائن
-                        MyUser myUser = new MyUser();
-                        //تحديد قيم الصفات بالقيم التي استخرجناها
-                        myUser.email = email;
-                        myUser.fullName = name;
-                        myUser.phone = phone;
-                        myUser.passw = password;
-                        //اضافه الكائن الجديد للجدول
-                        usersQuery.insert(myUser);
-                        //اغلاق الشاشه الحالية
-                        finish();
+        }
 
 
-                    }
-                }
+        if (isAllok) {
+            Toast.makeText(this, "All Ok", Toast.LENGTH_SHORT).show();
+            AppDatabase db = AppDatabase.getDB(getApplicationContext());
+            MyUserQuery usersQuery = db.getMyUserQuery();
+            //فحص هل البريد الالكتروني موجود من قبل اي تم تسجيل من قبل
+            if (usersQuery.checkEmail(email) != null) {
+                etEmail.setError("found Email");
+            } else// ان لم يكن موجودا نقوم ببناء كاءن للمستعمل وادخاله في الجدول Myuser المستعملين
+            {
+                // بناء الكائن
+                MyUser myUser = new MyUser();
+                //تحديد قيم الصفات بالقيم التي استخرجناها
+                myUser.email = email;
+                myUser.fullName = name;
+                myUser.phone = phone;
+                myUser.passw = password;
+                //اضافه الكائن الجديد للجدول
+                usersQuery.insert(myUser);
+                //اغلاق الشاشه الحالية
+                finish();
+
+
             }
-    public void onclickCancel(View v){
+        }
+    }
+
+    public void onclickCancel(View v) {
 
         finish();
-
     }
-
-
-        }
-
+}
 
 
 
