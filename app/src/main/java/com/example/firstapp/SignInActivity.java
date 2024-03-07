1e40a7b18e56521f34d80583dc1a49cf879c33cf
Admin <ibraheemafifi02@gmail.com>
2024-03-07 09:41:18 +0200

    fgh

--- app/src/main/java/com/example/firstapp/SignInActivity.java
+++ app/src/main/java/com/example/firstapp/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.firstapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,10 +12,13 @@ import android.widget.Toast;
 import com.example.firstapp.data.AppDatabase;
 import com.example.firstapp.data.usersTable.MyUser;
 import com.example.firstapp.data.usersTable.MyUserQuery;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class SignInActivity extends AppCompatActivity
-{
+public class SignInActivity extends AppCompatActivity {
 
 
     private TextInputEditText etEmail;//الايميل الشخصي
@@ -26,23 +30,24 @@ public class SignInActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        etPassword=findViewById(R.id.etPassword);
-        etEmail=findViewById(R.id.etEmail);
-        btnSingUp2=findViewById(R.id.btnSingUp2);
-        btnSignIn1=findViewById(R.id.btnSignIn1);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
+        btnSingUp2 = findViewById(R.id.btnSingUp2);
+        btnSignIn1 = findViewById(R.id.btnSignIn1);
     }
 
-    public void onclickSignUp(View v)
-    {
+    public void onclickSignUp(View v) {
 
         Intent i = new Intent(SignInActivity.this, SignUpActivity.class);
         startActivity(i);
     }
-    public  void onclicksignIn(View v)
-    {
-        checkEmailPassw();
+
+    public void onclicksignIn(View v) {
+
+        checkEmailPassw_FB();
     }
-    private void    checkEmailPassw() {
+
+    private void checkEmailPassw() {
         boolean isAllok = true;
         String email = etEmail.getText().toString();
         String password = etPassword.getText().toString();
@@ -75,6 +80,41 @@ public class SignInActivity extends AppCompatActivity
             }
         }
     }
+         private void checkEmailPassw_FB() {
+            boolean isAllok = true;
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+            if (email.length() < 6 || email.contains("@") == false) {
+                isAllok = false;
+                etEmail.setError("Wrong Email");
+            }
+            if (password.length() < 8 || password.contains(" ") == true) {
+                isAllok = false;
+                etPassword.setError("Wrong password");
+            }
+            if (isAllok) {
+                Toast.makeText(this, "ALL OK", Toast.LENGTH_SHORT).show();
+            }
+            if (isAllok) {
+
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignInActivity.this, "Signing in", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(SignInActivity.this, MainActivity.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(SignInActivity.this, "Signing in failed", Toast.LENGTH_SHORT).show();
+                            etEmail.setError(task.getException().getMessage());
+                        }
 
 
-}
+                    }
+                });
+            }
+        }
+    }
+
+
