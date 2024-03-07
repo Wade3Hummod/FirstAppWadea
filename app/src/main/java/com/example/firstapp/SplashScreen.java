2cc14847b934c58d38a62518bd8e9bf6f9f8fbe9
Admin <ibraheemafifi02@gmail.com>
2024-03-07 12:20:25 +0200

    fgh

--- app/src/main/java/com/example/firstapp/SplashScreen.java
+++ app/src/main/java/com/example/firstapp/SplashScreen.java
@@ -20,8 +20,8 @@ public class SplashScreen extends AppCompatActivity
         setContentView(R.layout.activity_splash_screen);
         etWelcome=findViewById(R.id.etWelcome);
 
-        Log.d(" ", "OnCreate:");
-        Toast.makeText(this, "OnCreate:", Toast.LENGTH_SHORT).show();
+//        Log.d(" ", "OnCreate:");
+//        Toast.makeText(this, "OnCreate:", Toast.LENGTH_SHORT).show();
 
         //بناء قاعدة بيانات وارجاع مؤشر عليها 1
 
@@ -39,37 +39,37 @@ public class SplashScreen extends AppCompatActivity
         };
         h.postDelayed(r,3000);
     }
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("hummod", "onRestart: ");
-        Toast.makeText(this, "onRestart:",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("hummod", "onResume: ");
-        Toast.makeText(this, "onResume:",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("hummod", "onPause: ");
-        Toast.makeText(this, "onPause:",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("hummod", "onStop: ");
-        Toast.makeText(this, "onStop:",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("hummod", "onDestroy: ");
-        Toast.makeText(this, "onDestroy:",Toast.LENGTH_SHORT).show();
-    }
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        Log.d("hummod", "onRestart: ");
+//        Toast.makeText(this, "onRestart:",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Log.d("hummod", "onResume: ");
+//        Toast.makeText(this, "onResume:",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Log.d("hummod", "onPause: ");
+//        Toast.makeText(this, "onPause:",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Log.d("hummod", "onStop: ");
+//        Toast.makeText(this, "onStop:",Toast.LENGTH_SHORT).show();
+//    }
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Log.d("hummod", "onDestroy: ");
+//        Toast.makeText(this, "onDestroy:",Toast.LENGTH_SHORT).show();
+//    }
 }
